3914d41ff1aa1006d1a623df54c3957526004a00
Bati <94386708+BatiBati@users.noreply.github.com>
2024-12-28 11:59:31 +0800

    Card game started

---
+++ My card game/index.js
@@ -0,0 +1,29 @@
+const card = document.createElement("div");
+document.getElementById(card);
+document.getElementById("gameContainer").appendChild(card);
+const cardValues = [
+  "1",
+  "1",
+  "2",
+  "2",
+  "3",
+  "3",
+  "4",
+  "4",
+  "5",
+  "5",
+  "6",
+  "6",
+  "7",
+  "7",
+  "8",
+  "8",
+];
+console.log(cardValues);
+
+function cardShuffle (shuffle) {
+// for ()
+
+} 
+ cardShuffle(cardValues);
+
