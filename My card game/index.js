e1719d838ce2dc7792d5e5e939a3459799ab2409
Bati <94386708+BatiBati@users.noreply.github.com>
2024-12-29 08:51:50 +0800

    a

--- My card game/index.js
+++ My card game/index.js
@@ -1,35 +1,41 @@
-const card = document.createElement("div");
-document.getElementById(card);
-document.getElementById("gameContainer").appendChild(card);
-const cardValues = [
-  "1",
-  "1",
-  "2",
-  "2",
-  "3",
-  "3",
-  "4",
-  "4",
-  "5",
-  "5",
-  "6",
-  "6",
-  "7",
-  "7",
-  "8",
-  "8",
-];
+// const gameContainer = document.getElementById("gameContainer");
 
-function cardShuffle (shuffle) {
-for (let i = shuffle.length - 1; i >= 0; i--) {
-  console.log(shuffle[i]);
-  const j = Math.floor(Math.random()i+1);
+// const cardValue = [
+//   "🏀",
+//   "🏀",
+//   "🏈",
+//   "🏈",
+//   "⚾️",
+//   "⚾️",
+//   "🥎",
+//   "🥎",
+//   "🎱",
+//   "🎱",
+//   "🥊",
+//   "🥊",
+//   "🏓",
+//   "🏓",
+//   "🎲",
+//   "🎲",
+// ];
 
+// function cardShuffle(shuffle) {
+//   for (let i = shuffle.length - 1; i >= 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [shuffle[i], shuffle[j]] = [shuffle[j], shuffle[i]];
+//   }
+//   console.log(shuffle);
+//   return shuffle;
+// }
+// cardShuffle(cardValue);
 
-}
+let numbers = [1, 2, 3, 4, 5];
 
-} 
- cardShuffle(cardValues);
+let modifiedArray = numbers.map((number, index, array) => {
+  return `Element at index ${index}: ${number * 2}`;
+});
 
- 
- console.log(j);
+console.log(modifiedArray);
+
+let listHTML = "<ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>";
+console.log(listHTML);
